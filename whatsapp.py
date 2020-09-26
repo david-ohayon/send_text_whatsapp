60ad4b95e3cf18a274144b7cae1ceb75388573d5
David Ohayon <davideytanohayon@gmail.com>
2020-09-26 22:19:49 +0300

    first commit

---
+++ whatsapp.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from os import system
+from time import sleep
+
+
+def txt_to_wordlist():
+    with open('script.txt') as file:
+        list = [line.strip() for line in file]
+        words = " ".join(list)
+        return words.split()
+
+
+profile_path = '--user-data-dir=/Users/davidohayon/Library/Application Support/Google/Chrome/'
+
+system("osascript -e \'quit app \"Google Chrome\"\'")
+
+name = input('Enter the name of user or group: ')
+
+options = webdriver.ChromeOptions()
+options.add_argument(profile_path)
+driver = webdriver.Chrome(options=options)
+
+sleep(1)
+driver.get('https://web.whatsapp.com/')
+
+msg = txt_to_wordlist()
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+user.click()
+msg_box = driver.find_elements_by_class_name('_3FRCZ')[1]
+
+for i in range(len(txt_to_wordlist())):
+    msg_box.send_keys(msg[i])
+    msg_box.submit()
+    # button = driver.find_element_by_class_name('_1U1xa')
+    # button.click()
+    sleep(.2)
